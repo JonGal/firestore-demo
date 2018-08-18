57b087003f1bc149acd69890f937e5000f9157f9
Jon Gallagher <jon@nubedehelado.com>
2018-08-18 11:27:05 -0700

    Strip out pointer to key from imports.js

--- imports.js
+++ imports.js
@@ -1,6 +1,6 @@
 
-//jon@mode2.com key
-var serviceAccount = require('/Users/jongallagher/src/firestore-demo/keys/fir-firestore-demo-3915d-firebase-adminsdk-cd1r1-efe257a873.json');
-//jon.gallagher@gmail.com key
-//var serviceAccount = require('/Users/jongallagher/src/firestore-demo/keys/firestore-backup-demo-firebase-adminsdk-nl3qh-db4dce1a9c.json');
+//Private key from Firebase Console
+//Geat -> Users and Permissons, Service Accounts->Firebase Admin SDK, Generate New Private key
+var serviceAccount = require('/path/to/keyfile.json');
+
 module.exports = { serviceAccountKey :  serviceAccount};
